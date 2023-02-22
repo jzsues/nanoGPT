ba1ea1e286c5ea36afb73dedabc007b0276f21dc
jiang <jzsues@gmail.com>
2023-02-22 08:51:55 +0800

    add chinese mini gpt model trainer

--- data/chinese_poetry/prepare_v2.py
+++ data/chinese_poetry/prepare_v2.py
@@ -2,16 +2,28 @@ import os
 import requests
 import tiktoken
 import numpy as np
+import pickle
 
 # download the test chinese poetry dataset
 
 input_file_path = os.path.join(os.path.dirname(__file__), 'test_chinese_poetry_v1.txt')
 
-with open(input_file_path, 'r') as f:
-    data = f.read()
-n = len(data)
-train_data = data[:int(n * 0.9)]
-val_data = data[int(n * 0.9):]
+
+def read_raw_file_lines(raw_data_path):
+    with open(raw_data_path, 'r', encoding='utf-8') as f:
+        print('reading lines')
+        rlines = f.readlines()
+        rlines = [line.replace('\n', '') for line in rlines]
+    rlines_len = len(rlines)
+    return rlines, rlines_len
+
+
+min_length = 16
+lines, lines_len = read_raw_file_lines(input_file_path)
+
+split_pos = int(lines_len * 0.9)
+train_lines = lines[:split_pos]
+val_lines = lines[split_pos:]
 
 # encode with tiktoken gpt2 bpe
 # enc = tiktoken.get_encoding("gpt2")
@@ -22,8 +34,24 @@ from tokenizations import tokenization_bert
 vocab_file_path = os.path.join(os.path.dirname(__file__), '../../cache/vocab_all.txt')
 tokenizer = tokenization_bert.BertTokenizer(vocab_file=vocab_file_path)
 tokenizer.max_len = 999999
-train_ids = tokenizer.encode(train_data)
-val_ids = tokenizer.encode(val_data)
+train_sublines = [tokenizer.tokenize(line) for line in train_lines if len(line) > min_length]
+val_sublines = [tokenizer.tokenize(line) for line in val_lines if len(line) > min_length]
+train_sublines = [tokenizer.convert_tokens_to_ids(line) for line in train_sublines]
+val_sublines = [tokenizer.convert_tokens_to_ids(line) for line in val_sublines]
+train_ids = []
+val_ids = []
+for subline in train_sublines:
+    train_ids.append(tokenizer.convert_tokens_to_ids('[MASK]'))  # 文章开头添加MASK表示文章开始
+    train_ids.extend(subline)
+    train_ids.append(tokenizer.convert_tokens_to_ids('[SEP]'))  # 文章结束添加SEP表示文章结束
+    train_ids.append(tokenizer.convert_tokens_to_ids('[CLS]'))  # 文章之间添加CLS表示文章结束
+
+for subline in val_sublines:
+    val_ids.append(tokenizer.convert_tokens_to_ids('[MASK]'))  # 文章开头添加MASK表示文章开始
+    val_ids.extend(subline)
+    val_ids.append(tokenizer.convert_tokens_to_ids('[SEP]'))  # 文章结束添加SEP表示文章结束
+    val_ids.append(tokenizer.convert_tokens_to_ids('[CLS]'))  # 文章之间添加CLS表示文章结束
+
 print(f"vocab size:{tokenizer.vocab_size}")
 print(f"train has {len(train_ids):,} tokens")
 print(f"val has {len(val_ids):,} tokens")
@@ -34,5 +62,9 @@ val_ids = np.array(val_ids, dtype=np.uint16)
 train_ids.tofile(os.path.join(os.path.dirname(__file__), 'train.bin'))
 val_ids.tofile(os.path.join(os.path.dirname(__file__), 'val.bin'))
 
-# train.bin has 301,966 tokens
-# val.bin has 36,059 tokens
+# save the meta information as well, to help us encode/decode later
+meta = {
+    'vocab_size': tokenizer.vocab_size,
+}
+with open(os.path.join(os.path.dirname(__file__), 'meta.pkl'), 'wb') as f:
+    pickle.dump(meta, f)
